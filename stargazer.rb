84d65277ef5467d20a7ede091754926f1fae00ff
Daniel Black <dyspop@gmail.com>
2016-02-14 15:10:28 -0500

    log results only

--- stargazer.rb
+++ stargazer.rb
@@ -7,6 +7,9 @@ while true
   d = Random.new.rand(0..255).to_s
   ip = a + '.' + b + '.' + c + '.' + d
   print ip
-  `echo "#{ip}" >> stargazer.log`
-  `curl -I #{ip} --anyauth --connect-timeout 1 --max-time 3 >> stargazer.log`
+  star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s
+  if star != ''
+    `echo "#{ip}" >> stargazer.log`
+    `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3 >> stargazer.log`
+  end
 end
