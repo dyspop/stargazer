05d3e2c79a95b8aa32c1cf485a3cb14e64c529d7
Daniel Black <dyspop@gmail.com>
2016-02-15 17:00:13 -0500

    revise construction of ip address

--- stargazer.rb
+++ stargazer.rb
@@ -1,11 +1,7 @@
 `touch stargazer.log`
 
 while true
-  a = Random.new.rand(0..255).to_s
-  b = Random.new.rand(0..255).to_s
-  c = Random.new.rand(0..255).to_s
-  d = Random.new.rand(0..255).to_s
-  ip = a + '.' + b + '.' + c + '.' + d
+  ip = ([proc { Random.rand(0..255) }]*4).map(&:call).join('.')
   print ip
   star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s
   if star != ''
