d4c9fc36433981b6967ef5ddf4ebd05150adb5c6
drew cimino <dscimino@gmail.com>
2016-02-15 20:29:51 -0500

    more terribler

--- stargazer.rb
+++ stargazer.rb
@@ -1,11 +1,5 @@
 `touch stargazer.log`
 
-while true
-  ip = ([proc { Random.rand(0..255) }]*4).map(&:call).join('.')
-  print ip
-  star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s
-  if star != ''
-    `echo "#{ip}" >> stargazer.log`
-    `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3 >> stargazer.log`
-  end
+while (puts ip = ([proc { Random.rand(0..255) }]*4).map(&:call).join('.')) || ip
+  `echo "#{ip + "\n" + @star}" >> stargazer.log` unless (@star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s).empty?
 end
