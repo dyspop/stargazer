6b0b7e7e327eed10e0c0e6ac7a6b43d1ad6b0baa
Daniel Black <dyspop@gmail.com>
2016-02-16 14:52:48 -0500

    log date and open browser on 200 OK

--- stargazer.rb
+++ stargazer.rb
@@ -5,7 +5,12 @@ while true
   print ip
   star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s
   if star != ''
+    `date >> stargazer.log`
     `echo "#{ip}" >> stargazer.log`
     `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3 >> stargazer.log`
+  end
+  #pattern match for other protocols and version numbers here would be great, I just suck at regex:
+  if star.include? '200 OK'
+    `open http://#{ip} -g`
   end
 end
