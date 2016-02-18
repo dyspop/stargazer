74319076592ae0b3de9b4357991600a28c6d307f
Daniel Black <dyspop@gmail.com>
2016-02-17 23:43:19 -0500

    commented testing ip and logic for following 301/302s

--- stargazer.rb
+++ stargazer.rb
@@ -2,6 +2,7 @@
 
 while true
   ip = ([proc { Random.rand(0..255) }]*4).map(&:call).join('.')
+  #ip = '200.77.168.145' #testing ip
   print ip
   star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s
   if star != ''
@@ -12,5 +13,8 @@ while true
   #pattern match for other protocols and version numbers here would be great, I just suck at regex:
   if star.include? '200 OK'
     `open http://#{ip} -g`
+  elsif (star.include? '301 Moved' or star.include? '302 Moved') and star.include? ('Location')
+    #do a new one
   end
+
 end
