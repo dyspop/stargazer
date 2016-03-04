1eac486d3fa130473e6e1923842af61a581a9a55
Daniel Black <dyspop@gmail.com>
2016-03-03 21:33:19 -0500

    add nslookup to logs

--- stargazer.rb
+++ stargazer.rb
@@ -1,22 +1,25 @@
+require 'geoip'
 `touch stargazer.log`
 
 while true
   ip = ([proc { Random.rand(0..255) }]*4).map(&:call).join('.')
-  #ip = '200.251.23.49' #testing ip
-  print ip
+  #ip = '73.0.201.233' #testing ip
   star = `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3`.to_s
   if star != ''
     `date >> stargazer.log`
     `echo "#{ip}" >> stargazer.log`
+    `echo nslookup #{ip}`
     `curl -I #{ip} --anyauth --connect-timeout 0.1 --max-time 3 >> stargazer.log`
   end
   #pattern match for other protocols and version numbers here would be great, I just suck at regex:
   if star.include? '200 OK'
     `open http://#{ip} -g`
-  elsif (star.include? '301 Moved' or star.include? '302 Moved') and star.include? 'Location'
+  elsif (star.include? '301 Moved' or star.include? '302 Moved' or star.include? '301 Redirect') and star.include? 'Location'
     moved_location = star.split('ocation: ')
+    print moved_location
     moved_location_uri = moved_location[1].split("\n")
+    print moved_location_uri
     `open #{moved_location_uri[0]} -g`
   end
-  print "\n\n\n" + star + "\n\n\n"
+  #break
 end
